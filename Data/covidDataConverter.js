8b40986be196f9b2ed520b8fb3bec31e20867e81
Sorada Prathan <sprathan@my.centennialcollege.ca>
2024-04-02 20:34:22 -0400

    fix issue on covid data conversion, closes #19

--- Data/covidDataConverter.js
+++ Data/covidDataConverter.js
@@ -1,35 +1,3 @@
-// const fs = require("fs");
-// csv = fs.readFileSync("./Covid Dataset.csv");
-
-// array = csv.toString().split(/\r?\n/);
-
-// let result = [];
-
-// let headers = array[0].split(",");
-
-// console.log(headers);
-// for (let i = 0; i < headers.length; i++) {
-//   headers[i] = headers[i].replace("'", "");
-// }
-
-// for (let i = 1; i < array.length - 1; i++) {
-//   let dataLine = array[i].split(",");
-//   obj = {};
-//   for (let j = 0; j < headers.length; j++) {
-//     if (!isNaN(dataLine[j])) {
-//       dataLine[j] = +dataLine[j];
-//     }
-//     obj[headers[j]] = dataLine[j];
-//   }
-
-//   result.push(obj);
-// }
-
-// console.log(result);
-
-// // let json = JSON.stringify(result)
-
-// fs.writeFileSync("covidDataConverted.json", JSON.stringify(result));
 const fs = require("fs");
 
 // Read the CSV file
